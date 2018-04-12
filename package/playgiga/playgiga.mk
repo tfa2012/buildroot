65726ac5c3ce49a2cf820b4d851ca118a996966d
Sander van der Maar <sandervandermaar@gmail.com>
2018-04-12 17:13:12 +0200

    [PlayGiga] Fixes compilation of PlayGiga client. - Bumps GCC - Adds PlayGiga Config/makefile

---
+++ package/playgiga/playgiga.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# playgiga
+#
+################################################################################
+
+PLAYGIGA_VERSION = aee6ad5bd8da2b6ce8011e048cf9ba161523059c
+PLAYGIGA_SITE = git@github.com:Metrological/playgiga.git
+PLAYGIGA_SITE_METHOD = git
+PLAYGIGA_LICENSE = PROPRIETARY
+PLAYGIGA_DEPENDENCIES = sdl2 ffmpeg openal libcurl
+PLAYGIGA_INSTALL_TARGET = YES
+PLAYGIGA_SUBDIR = arm32 # TODO: let it depend on arch
+
+$(eval $(cmake-package))
+

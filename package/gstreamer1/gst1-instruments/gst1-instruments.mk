8b3a8bafd78bb2cbe75d28337d7a93fd32b137fd
Enrique Ocaña González <eocanha@igalia.com>
2018-05-21 16:53:27 +0000

    GStreamer packages version upgrade from 1.10.4 to 1.14.1
    for gst-omx, gst1-common, gst1-instruments, gst1-libav, gst1-plugins-{base,good,bad,ugly},
    gst1-rstp-server, gst1-vaapi, gst1-validate, gstreamer1

--- package/gstreamer1/gst1-instruments/gst1-instruments.mk
+++ package/gstreamer1/gst1-instruments/gst1-instruments.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_INSTRUMENTS_VERSION = 2c69d0f685b2276493d0bb05ca83bf03a18233dd
+GST1_INSTRUMENTS_VERSION = 0.2.3
 GST1_INSTRUMENTS_SITE = $(call github,kirushyk,gst-instruments,$(GST1_INSTRUMENTS_VERSION))
 GST1_INSTRUMENTS_LICENSE = LGPLv2.1+
 GST1_INSTRUMENTS_LICENSE_FILES = COPYING

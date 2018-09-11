9f489c0d9a991f87362dcb219d9446d70b1c44dc
Sander van der Maar <sandervandermaar@gmail.com>
2018-09-11 18:44:17 +0200

    [NETFLIX5] Build test players

--- package/netflix5/netflix5.mk
+++ package/netflix5/netflix5.mk
@@ -35,15 +35,18 @@ NETFLIX5_CONF_OPTS = \
 	-DNRDP_CRASH_REPORTING="off" \
 	-DNRDP_TOOLS="provisioning" \
 	-DDPI_IMPLEMENTATION=sink-interface \
-	-DDPI_SINK_INTERFACE_IMPLEMENTATION=null \
+	-DDPI_SINK_INTERFACE_IMPLEMENTATION=gstreamer \
 	-DBUILD_DEBUG=OFF -DNRDP_HAS_GIBBON_QA=ON -DNRDP_HAS_MUTEX_STACK=ON -DNRDP_HAS_OBJECTCOUNT=ON \
 	-DBUILD_PRODUCTION=OFF -DNRDP_HAS_QA=ON -DBUILD_SMALL=OFF -DBUILD_SYMBOLS=ON -DNRDP_HAS_TRACING=OFF \
 	-DNRDP_CRASH_REPORTING=breakpad \
 	-DNRDP_HAS_AUDIOMIXER=OFF \
 	-DDPI_SINK_INTERFACE_OVERRIDE_APPBOOT=ON \
 	-DGIBBON_GRAPHICS=rpi \
-	-DGIBBON_GRAPHICS_GL_WSYS=egl
-
+	-DGIBBON_GRAPHICS_GL_WSYS=egl \
+	-DNETFLIX5_BUILD_PLAYER01=ON \
+	-DNETFLIX5_BUILD_PLAYER02=ON \
+	-DNETFLIX5_BUILD_PLAYER03=ON
+	
 ifeq ($(BR2_PACKAGE_NETFLIX5_LIB), y)	
 NETFLIX5_CONF_OPTS += -DGIBBON_MODE=shared
 NETFLIX5_FLAGS = -O3 -fPIC
@@ -214,8 +217,6 @@ NETFLIX5_CONF_OPTS += \
 	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) $(NETFLIX5_FLAGS)"
 
 define NETFLIX5_INSTALL_STAGING_CMDS
-   echo 'About to start install staging commands'
-
 	make -C $(@D)/netflix install
 	$(INSTALL) -m 755 $(@D)/netflix/src/platform/gibbon/libnetflix.so $(STAGING_DIR)/usr/lib
 	$(INSTALL) -D package/netflix/netflix.pc $(STAGING_DIR)/usr/lib/pkgconfig/netflix.pc
@@ -253,13 +254,21 @@ define NETFLIX5_INSTALL_STAGING_CMDS
 	cp $(@D)/netflix/src/platform/gibbon/resources/gibbon/icu/icudt58l/debug/unames.icu $(TARGET_DIR)/root/Netflix/icu/icudt58l
 	cp $(@D)/netflix/src/platform/gibbon/*.js* $(TARGET_DIR)/root/Netflix/resources/js
 	cp $(@D)/netflix/src/platform/gibbon/resources/default/PartnerBridge.js $(TARGET_DIR)/root/Netflix/resources/js
-
-   echo 'Done with install staging commands'
+	
+	$(INSTALL) -m 755 $(@D)/netflix/tests_binary_output/player01/netflix5_player01 $(STAGING_DIR)/usr/bin
+	$(INSTALL) -m 755 $(@D)/netflix/tests_binary_output/player02/netflix5_player02 $(STAGING_DIR)/usr/bin
+	$(INSTALL) -m 755 $(@D)/netflix/tests_binary_output/player03/netflix5_player03 $(STAGING_DIR)/usr/bin
 endef
 
 define NETFLIX5_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 $(@D)/netflix/src/platform/gibbon/libnetflix.so $(TARGET_DIR)/usr/lib
 	$(STRIPCMD) $(TARGET_DIR)/usr/lib/libnetflix.so
+	$(INSTALL) -m 755 $(@D)/netflix/tests_binary_output/player01/netflix5_player01 $(TARGET_DIR)/usr/bin
+	$(STRIPCMD) $(TARGET_DIR)/usr/bin/netflix5_player01
+	$(INSTALL) -m 755 $(@D)/netflix/tests_binary_output/player02/netflix5_player02 $(TARGET_DIR)/usr/bin
+	$(STRIPCMD) $(TARGET_DIR)/usr/bin/netflix5_player02
+	$(INSTALL) -m 755 $(@D)/netflix/tests_binary_output/player03/netflix5_player03 $(TARGET_DIR)/usr/bin
+	$(STRIPCMD) $(TARGET_DIR)/usr/bin/netflix5_player03
 endef
 
 define NETFLIX5_PREPARE_DPI

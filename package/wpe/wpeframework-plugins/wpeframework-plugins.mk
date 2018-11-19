8921b09fe1f08c694b553f529dcdc9a4de2abc66
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2018-11-19 13:24:19 +0100

    [wpeframework-plugins] Sync with vss rdk

--- package/wpe/wpeframework-plugins/wpeframework-plugins.mk
+++ package/wpe/wpeframework-plugins/wpeframework-plugins.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_PLUGINS_VERSION = fde7e5bd47f33cb37a7163005e2d125ffd02b2b6
+WPEFRAMEWORK_PLUGINS_VERSION = bc64f19ac6f965c51f71efd9671b66de2f74de3d
 WPEFRAMEWORK_PLUGINS_SITE = $(call github,WebPlatformForEmbedded,WPEFrameworkPlugins,$(WPEFRAMEWORK_PLUGINS_VERSION))
 WPEFRAMEWORK_PLUGINS_INSTALL_STAGING = YES
 WPEFRAMEWORK_PLUGINS_DEPENDENCIES = wpeframework libpng
